6af2a91ad6508c6f122763bc87e8a8ce4b8f7f14
emok032 <emok032@gmail.com>
2016-10-17 20:30:56 -0400

    WIP Save

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,15 @@
 exports.signup = function(req, res, next) {
-		
+	
+	console.log(req.body);
+	const email = req.body.email;
+	const password = req.body.password;
+
+	// Verify: email is unique
+
+	// Action (-): If duplicate exists, RETURN ERROR
+
+	// Action (+): If unique email entry, CREATE & SAVE new user record
+
+	// Response--> Request: Confirm User Created
+
 }
--- server/router.js
+++ server/router.js
@@ -1,10 +1,11 @@
+const Authentication = require('../controllers/authentication');
+
 module.exports = function(app) {
 
 // request: incoming
 // response: outgoing (what handle in response to user request)
 // next: for error handling
 
-	app.get('/', function(req, res, next) {
-		res.send(['I', 'received', 'four', 'objects']);
-	});
+	app.post('/signup', Authentication.signup);
+		// function Authentication (see controller)
 }
