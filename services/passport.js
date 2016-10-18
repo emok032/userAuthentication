d0b63775373791a9fc4c2921d1b6bd0ecbf2ca95
emok032 <emok032@gmail.com>
2016-10-17 22:34:06 -0400

    WIP save

--- services/passport.js
+++ services/passport.js
@@ -8,18 +8,30 @@ const ExtractJwt = require('passoport-jwt').ExtractJwt;
 // *Passport-Strategy(s): Method(s) for authenticating a user
 // (i.e. via JWT or username/password)
 
-//  SETTING UP options for JWT Strategy-------------------------------------
+//  SETTING UP options for JWT Strategy-------------------------------------------------------------------
 const jwtOptions = {};
 
-//  CREATING JWT Strategy---------------------------------------------------
+//  CREATING JWT Strategy---------------------------------------------------------------------------------
 // 	------(Decoded JWT Token) 'payload' - (see controllers/authentication.js)
 const jwtLogin = new JwtStrategy(jwtOptions, function({payload, done) {
-	// See if user ID IN PAYLOAD exist in DB:
+	// See if user ID IN PAYLOAD exist in DB--------------------------------------------------------------
 	// 		(+): call 'done' WITH a user object
 	// 		(-): call 'done' WITHOUT a user object
 	User.findById(payload.sub, function(err, user) {
+		// if WITH ERROR (no - user object)
 		if (err) { 
-			return done(err, false); 
+			// 'unsuccessful' search process(err): return error object;
+			return done(err, false);
+			// err (returning error object)
+			// false (user object if we did NOT found one)
+		}
+		// if WITHOUT ERROR (yes - user object)
+		if (user) {
+			// 'successful' search process(null): return (user); no error object to return;
+			done(null, user);
+		} else {
+			// 'successful' search process(null) - but user NOT found (false); no error object to return;
+			done(null, false);
 		}
 	});
 });
