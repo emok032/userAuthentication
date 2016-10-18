30e0fffb6f4b8546582154beb6e7d71e69fc5393
emok032 <emok032@gmail.com>
2016-10-17 20:45:18 -0400

    WIP Save

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -6,15 +6,40 @@ exports.signup = function(req, res, next) {
 	const email = req.body.email;
 	const password = req.body.password;
 
-	// Verify: email is unique
+	// Verify: email is unique in database
 	// Will search for email-->Invoke a Callback to be passed (err, existingUser) 
 	User.findOne({ email: email }, function(err, existingUser) {
+		if  (err) {
+			return next(err);
+		}
+
+		// Action (-): If DUPLICATE exists, RETURN ERROR
+		if (existingUser) {
+			return res.status(422).send({ error: 'Email is in use' });
+			// Error 422: Unprocessable Request
+		}
+
+	
+
+		// Action (+): If UNIQUE email entry, CREATE & SAVE new user record
 		
-	})
-	// Action (-): If duplicate exists, RETURN ERROR
+		// *ONLY CREATING User (not saved yet)
+		const user = new User({
+			email: email,
+			password: password
+			// **ONLY Creates User (not saved yet)
+		});
 
-	// Action (+): If unique email entry, CREATE & SAVE new user record
+		// *Now SAVING User to database
+		user.save(function(err) {
+			if (err) {
+				return next(err);
+			}
 
-	// Response--> Request: Confirm User Created
+		// Response--> Request: Confirm User Created
+		res.json({ sucess: true });
+
+		});
+	});
 
 }
