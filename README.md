6e33758a9ae30ca9c5e27f314a87db0a5012f7a0
emok032 <emok032@gmail.com>
2016-10-17 20:54:37 -0400

    WIP save

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # userAuthentication
-Integrating user authentication with passportjs
+
+User Authentication:
+
+- To start: "npm run dev"
+- URL: Localhost://3000
+-Database: MongoDB
+-NPM:
+	"body-parser": "^1.15.2",
+    "express": "^4.14.0",
+    "mongoose": "^4.6.4",
+    "morgan": "^1.7.0",
+    "nodemon": "^1.11.0",
+    "bcrypt-nodejs": "0.0.3"
+
--- controllers/authentication.js
+++ controllers/authentication.js
@@ -6,8 +6,11 @@ exports.signup = function(req, res, next) {
 	const email = req.body.email;
 	const password = req.body.password;
 
+	if (!email || !password) {
+		return res.status(422).send({ error: 'Please provide both an email and password.'});
+	}
 	// Verify: email is unique in database
-	// Will search for email-->Invoke a Callback to be passed (err, existingUser) 
+	// Search for email--> Invoke a Callback to be passed (err, existingUser) 
 	User.findOne({ email: email }, function(err, existingUser) {
 		if  (err) {
 			return next(err);
@@ -16,7 +19,7 @@ exports.signup = function(req, res, next) {
 		// Action (-): If DUPLICATE exists, RETURN ERROR
 		if (existingUser) {
 			return res.status(422).send({ error: 'Email is in use' });
-			// Error 422: Unprocessable Request
+			// (Error 422: Unprocessable Request)
 		}
 
 	
@@ -27,7 +30,6 @@ exports.signup = function(req, res, next) {
 		const user = new User({
 			email: email,
 			password: password
-			// **ONLY Creates User (not saved yet)
 		});
 
 		// *Now SAVING User to database
@@ -35,11 +37,10 @@ exports.signup = function(req, res, next) {
 			if (err) {
 				return next(err);
 			}
-
 		// Response--> Request: Confirm User Created
 		res.json({ sucess: true });
-
 		});
+
 	});
 
 }
